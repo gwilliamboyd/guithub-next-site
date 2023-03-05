0b96c0f40935d929b11613d7f2b1ce88b7805566
gwilliamboyd <gwillboyd@outlook.com>
2023-03-05 12:10:51 -0600

    Begins full design of [slug] page for FX

--- pages/effects/[slug]/index.jsx
+++ pages/effects/[slug]/index.jsx
@@ -20,32 +20,44 @@ const Effect = ({ effect }) => {
 
 	return (
 		<div className={productStyles.productMaster}>
+			<p className={productStyles.productHeading}>{effect.name}</p>
 			<div className={productStyles.productBody}>
-				<p className={productStyles.productHeading}>{effect.name}</p>
-				<Image
-					src={urlFor(effect.image).url()}
-					width={0}
-					height={0}
-					alt={effect.name}
-					sizes='100vw'
-					style={{ width: '300px', height: 'auto' }}
-				/>
-				<button
-					className={productStyles.addToCart}
-					onClick={() => onAdd(effect, 1)}>
-					Add To Cart
-				</button>
-				<h2>Product Description</h2>
-				<p className={productStyles.prodDesc}>
-					{effect.productDescription[0].children[0].text}
-				</p>
-				{/* <p>{Object.values(effect.productDescription)}</p> */}
-				<p>
-					<b>Body Material: </b>
-					{effect.bodyMaterial}
-				</p>
-				{/* Need to find property to display for block text */}
+				<div className={productStyles.bodyImages}>
+					<Image
+						src={urlFor(effect.image).url()}
+						width={0}
+						height={0}
+						alt={effect.name}
+						sizes='100vw'
+						style={{ width: '300px', height: 'auto' }}
+					/>
+				</div>
+				<div className={productStyles.bodyText}>
+					<div className={productStyles.bodyRowOne}>
+						<span className={productStyles.ourPrice}>Our Price:</span>
+						<span className={productStyles.price}>{`$${effect.price}`}</span>
+					</div>
+					<div className={productStyles.bodyRowTwo}>
+						<span className={productStyles.ourRating}>Rating:</span>
+					</div>
+					<div className={productStyles.bodyRowThree}></div>
+					<div className={productStyles.bodyRowFour}></div>
+					<button
+						className={productStyles.addToCart}
+						onClick={() => onAdd(effect, 1)}>
+						Add To Cart
+					</button>
+
+					<p>
+						<b>Body Material: </b>
+						{effect.bodyMaterial}
+					</p>
+				</div>
 			</div>
+			<h2>Product Description</h2>
+			<p className={productStyles.prodDesc}>
+				{effect.productDescription[0].children[0].text}
+			</p>
 		</div>
 	)
 }
