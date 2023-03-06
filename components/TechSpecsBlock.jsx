60065351bbc1671fda0e03706174e04efa81193b
gwilliamboyd <gwillboyd@outlook.com>
2023-03-05 22:26:04 -0600

    Tech Specs row formatted properly
    Tech specs auto-generate as expected, and names are fully capitalized

--- components/TechSpecsBlock.jsx
+++ components/TechSpecsBlock.jsx
@@ -1,14 +1,38 @@
 const TechSpecsBlock = ({ effect, productStyles }) => {
+	// Puts Sanity tech specs array into an array readable by component
 	const techSpecsArray = Object.entries(effect.techSpecs)
-	console.log(techSpecsArray[0][0])
+
+	// Formats key names from Sanity to be readable on Tech Specs table
+	// This helps to automatically generate all the names
+	const formatKey = s => {
+		const spaceResult = s.replace('_', ' ')
+
+		const finalResult = spaceResult.split(' ')
+		for (let i = 0; i < finalResult.length; i++) {
+			finalResult[i] = finalResult[i][0].toUpperCase() + finalResult[i].slice(1)
+		}
+		const result = finalResult.join(' ')
+		console.log(result)
+		return result
+	}
+	// Formats value names for Tech Specs table
+	const capitalizeValue = s => {
+		const result = s.charAt(0).toUpperCase() + s.slice(1)
+		return result
+	}
+
 	return (
 		<div className={productStyles.techSpecsBlock}>
 			<span className={productStyles.techSpecsBlockHeading}>General</span>
 			<div className={productStyles.techSpecsTable}>
 				{techSpecsArray.map(spec => (
 					<div className={productStyles.techSpecRow}>
-						<span className={productStyles.rowHeading}>{spec[0]}</span>
-						<span className={productStyles.rowText}>{spec[1]}</span>
+						<span className={productStyles.rowHeading}>
+							{formatKey(spec[0])}
+						</span>
+						<span className={productStyles.rowText}>
+							{capitalizeValue(spec[1][0])}
+						</span>
 					</div>
 				))}
 			</div>
