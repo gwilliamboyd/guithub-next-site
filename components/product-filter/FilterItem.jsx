b56ae4e87b753a91dcc5ea21195e2502374201d9
gwilliamboyd <gwillboyd@outlook.com>
2023-03-08 13:03:28 -0600

    submenu opens on :hover again

--- components/product-filter/FilterItem.jsx
+++ components/product-filter/FilterItem.jsx
@@ -10,65 +10,67 @@ const FilterItem = ({ filterStyles, text, effects, query, setQuery }) => {
 				onMouseEnter={() => setMenuOpen(!menuOpen)}
 				onMouseLeave={() => setMenuOpen(!menuOpen)}>
 				{text}
-				<div className={filterStyles.subMenu}>
-					{query !== '' && (
-						<span className={filterStyles.subMenuItem}>
-							<span
-								className={filterStyles.subMenuItemText}
-								onClick={() => setQuery('')}>
-								Clear Filters
+				{menuOpen && (
+					<div className={filterStyles.subMenu}>
+						{query !== '' && (
+							<span className={filterStyles.subMenuItem}>
+								<span
+									className={filterStyles.subMenuItemText}
+									onClick={() => setQuery('')}>
+									Clear Filters
+								</span>
 							</span>
-						</span>
-					)}
-					<SubMenuItem
-						filterStyles={filterStyles}
-						text={'Distortion'}
-						query={query}
-						setQuery={setQuery}
-					/>
-					<SubMenuItem
-						filterStyles={filterStyles}
-						text={'Overdrive'}
-						query={query}
-						setQuery={setQuery}
-					/>
-					<SubMenuItem
-						filterStyles={filterStyles}
-						text={'Chorus'}
-						query={query}
-						setQuery={setQuery}
-					/>
-					<SubMenuItem
-						filterStyles={filterStyles}
-						text={'Phaser'}
-						query={query}
-						setQuery={setQuery}
-					/>
-					<SubMenuItem
-						filterStyles={filterStyles}
-						text={'Flanger'}
-						query={query}
-						setQuery={setQuery}
-					/>
-					<SubMenuItem
-						filterStyles={filterStyles}
-						text={'Wah'}
-						query={query}
-						setQuery={setQuery}
-					/>
-					<SubMenuItem
-						filterStyles={filterStyles}
-						text={'Reverb'}
-						query={query}
-						setQuery={setQuery}
-					/>
-					<SubMenuItem
-						filterStyles={filterStyles}
-						text={'Delay'}
-						query={query}
-						setQuery={setQuery}
-					/>
-				</div>
+						)}
+						<SubMenuItem
+							filterStyles={filterStyles}
+							text={'Distortion'}
+							query={query}
+							setQuery={setQuery}
+						/>
+						<SubMenuItem
+							filterStyles={filterStyles}
+							text={'Overdrive'}
+							query={query}
+							setQuery={setQuery}
+						/>
+						<SubMenuItem
+							filterStyles={filterStyles}
+							text={'Chorus'}
+							query={query}
+							setQuery={setQuery}
+						/>
+						<SubMenuItem
+							filterStyles={filterStyles}
+							text={'Phaser'}
+							query={query}
+							setQuery={setQuery}
+						/>
+						<SubMenuItem
+							filterStyles={filterStyles}
+							text={'Flanger'}
+							query={query}
+							setQuery={setQuery}
+						/>
+						<SubMenuItem
+							filterStyles={filterStyles}
+							text={'Wah'}
+							query={query}
+							setQuery={setQuery}
+						/>
+						<SubMenuItem
+							filterStyles={filterStyles}
+							text={'Reverb'}
+							query={query}
+							setQuery={setQuery}
+						/>
+						<SubMenuItem
+							filterStyles={filterStyles}
+							text={'Delay'}
+							query={query}
+							setQuery={setQuery}
+						/>
+					</div>
+				)}
 			</span>
 		</span>
 	)
