c49f9e1b8e204e3af5e5ae04078f3ffcde24ce42
gwilliamboyd <gwillboyd@outlook.com>
2023-03-15 16:12:47 -0500

    Home page/tech specs
    Tech specs on guitar [slug] now space and capitalize properly
    Index hero banner designed and built, motion to follow

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,14 @@ import homeStyles from '../styles/Home.module.css'
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import Navbar from '@/components/Navbar'
+import { motion } from 'framer-motion'
+// Hero Banner Imports
+// Guitars
+import redBgImg from '@/public/images/banner-guitar-red-bg.png'
+import blackBgImg from '@/public/images/banner-guitar-black-bg.png'
+import prsImg from '@/public/images/banner-guitar-prs.png'
+import explorerBgImg from '@/public/images/banner-guitar-explorer.png'
+import schecterBgImg from '@/public/images/banner-guitar-syn.png'
 
 export default function IndexPage({ products, guitars, amps }) {
 	const [gtrs, setGtrs] = useState(guitars)
@@ -15,10 +23,32 @@ export default function IndexPage({ products, guitars, amps }) {
 
 	return (
 		<div className={homeStyles.homeMaster}>
-			<h1 className={homeStyles.homeHeading}>Welcome to GuitHub!</h1>
-			<h5 className={homeStyles.tagline}>
-				We're <i>committed</i> to excellence!
-			</h5>
+			<div className={homeStyles.guitarHero}>
+				<motion.img
+					className={homeStyles.redBg}
+					src={redBgImg.src}
+				/>
+				<motion.img
+					className={homeStyles.blackBg}
+					src={blackBgImg.src}
+				/>
+				<motion.span className={homeStyles.guitarsText}>Guitars</motion.span>
+				<motion.span className={homeStyles.browseButton}>
+					Browse Here
+				</motion.span>
+				<motion.img
+					className={homeStyles.prs}
+					src={prsImg.src}
+				/>
+				<motion.img
+					className={homeStyles.explorer}
+					src={explorerBgImg.src}
+				/>
+				<motion.img
+					className={homeStyles.schecter}
+					src={schecterBgImg.src}
+				/>
+			</div>
 		</div>
 	)
 }
