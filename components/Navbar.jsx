94ca8527b4428591c273b4df1bf6497899d933a4
gwilliamboyd <gwillboyd@outlook.com>
2023-03-18 15:24:22 -0500

    svgr/webpack functionality in place

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,8 @@
 // Next.js utilities
 import Link from 'next/link'
 import Image from 'next/image'
+//React utilities
+import { useState } from 'react'
 // Components
 import NavbarLink from './NavbarLink'
 import SocialIcon from './SocialIcon'
@@ -12,13 +14,19 @@ import guithubLogo from '../public/images/guithub-logo-white.png'
 import instagramIcon from '@/public/images/instagram-icon-white.svg'
 import youtubeIcon from '@/public/images/youtube-icon-white.svg'
 import reverbIcon from '@/public/images/reverb-icon-white.svg'
-import shoppingCartIcon from '@/public/images/shopping-cart.svg'
+// import shoppingCartIcon from '@/public/images/shopping-cart.svg'
+import ShoppingCartIcon from './svgs/ShoppingCartIcon'
 import SearchBar from './SearchBar'
 import Cart from './Cart'
 import { useStateContext } from '@/context/StateContext'
 
 const Navbar = ({ products, guitars, amps }) => {
 	const { cartOpen, setCartOpen } = useStateContext()
+
+	//State
+	const [currentColor, setCurrentColor] = useState('var(--light-font-color)')
+	const [width, setWidth] = useState(30)
+	const [height, setHeight] = useState(30)
 
 	return (
 		<header className={navbarStyles.header}>
@@ -78,12 +86,29 @@ const Navbar = ({ products, guitars, amps }) => {
 						href='#'>
 						<a
 							className={navbarStyles.cartIcon}
-							onClick={() => setCartOpen(!cartOpen)}>
-							<Image
-								src={shoppingCartIcon}
+							onMouseEnter={() => {
+								setCurrentColor('blue')
+								setWidth(35)
+								setHeight(35)
+							}}
+							onMouseLeave={() => {
+								setCurrentColor('var(--light-font-color)')
+								setWidth(30)
+								setHeight(30)
+							}}
+							onClick={() => {
+								setCartOpen(!cartOpen)
+							}}>
+							{/* <Image
+								src={ShoppingCartIcon}
 								width={35}
 								height={35}
 								alt='View your cart'
+							/> */}
+							<ShoppingCartIcon
+								currentColor={currentColor}
+								width={width}
+								height={height}
 							/>
 						</a>
 					</Link>
--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
-/* const nextConfig = {
+const nextConfig = {
 	reactStrictMode: true,
-} */
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: '@svgr/webpack',
+		})
+	},
+}
 
 module.exports = {
-	// nextConfig,
+	nextConfig,
 	images: {
 		remotePatterns: [
 			{
@@ -14,6 +20,46 @@ module.exports = {
 				pathname: '/images/ggll893a/production/**',
 			},
 		],
+	},
+
+	// FROM JOSH TRIED CODING VIDEO
+
+	/* webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+		})
+		return config
+	}, */
+
+	// FROM SVGR WEBSITE
+
+	webpack(config) {
+		// Grab the existing rule that handles SVG imports
+		const fileLoaderRule = config.module.rules.find(rule =>
+			rule.test?.test?.('.svg')
+		)
+
+		config.module.rules.push(
+			// Reapply the existing rule, but only for svg imports ending in ?url
+			{
+				...fileLoaderRule,
+				test: /\.svg$/i,
+				resourceQuery: /url/, // *.svg?url
+			},
+			// Convert all other *.svg imports to React components
+			{
+				test: /\.svg$/i,
+				issuer: /\.[jt]sx?$/,
+				resourceQuery: { not: /url/ }, // exclude if *.svg?url
+				use: ['@svgr/webpack'],
+			}
+		)
+
+		// Modify the file loader rule to ignore *.svg, since we have it handled now.
+		fileLoaderRule.exclude = /\.svg$/i
+
+		return config
 	},
 	webpack: (config, { isServer }) => {
 		if (!isServer) {
