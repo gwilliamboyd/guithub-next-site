44fe7f133211d9f2941184284b0837620920ee8b
gwilliamboyd <gwillboyd@outlook.com>
2023-03-28 01:43:36 -0500

    mobile menu z index, ios shrinking fixed

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,9 @@ import Layout from '@/components/Layout'
 export default function App({ Component, pageProps }) {
 	return (
 		<StateContext>
+			<meta
+				name='viewport'
+				content='width=device-width, initial-scale=1'></meta>
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
