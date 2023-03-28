8e90db514b775e38096080e8d27d628ae7022cb5
gwilliamboyd <gwillboyd@outlook.com>
2023-03-28 02:02:16 -0500

    fixed ios shrinking issue

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,6 @@ export default function App({ Component, pageProps }) {
 			<meta
 				name='viewport'
 				content='width=device-width, initial-scale=1'></meta>
-			<title>GuitHub | "Commit"ted to excellence!</title>
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
