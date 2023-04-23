89f9e81b06d0ff7954e2aeb361cf02ae6e474710
gwilliamboyd <gwillboyd@outlook.com>
2023-04-23 17:24:20 -0500

    centered imgs in product cards, fixed image warp

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -13,35 +13,33 @@ export const ProductCard = ({ product, categoryStyles }) => {
 				as={`/${product._type}s/${product.slug.current}`}
 				legacyBehavior>
 				<a>
-					<div>
-						<Image
-							src={urlFor(product.image[0]).url()}
-							width={0}
-							height={0}
-							sizes='100vw'
-							style={{
-								maxWidth: '250px',
-								width: 'auto',
-								maxHeight: '350px',
-								height: 'auto',
-							}}
-							alt={product.name}
-							className={categoryStyles.productCardImage}
-						/>
-						<div className={categoryStyles.productCardBody}>
-							<b>{product.name}</b>
-							<div className={categoryStyles.productInfo}>
-								<b className={categoryStyles.productPrice}>${product.price}</b>
-								<span className={categoryStyles.productRating}>
-									{product.rating}/5{' '}
-									<Image
-										src={ratingIcon}
-										width={20}
-										height={20}
-										alt='Rating icon'
-									/>
-								</span>
-							</div>
+					<Image
+						src={urlFor(product.image[0]).url()}
+						width={0}
+						height={0}
+						sizes='100vw'
+						style={{
+							maxWidth: '250px',
+							width: 'auto',
+							maxHeight: '350px',
+							height: 'auto',
+						}}
+						alt={product.name}
+						className={categoryStyles.productCardImage}
+					/>
+					<div className={categoryStyles.productCardBody}>
+						<b>{product.name}</b>
+						<div className={categoryStyles.productInfo}>
+							<b className={categoryStyles.productPrice}>${product.price}</b>
+							<span className={categoryStyles.productRating}>
+								{product.rating}/5{' '}
+								<Image
+									src={ratingIcon}
+									width={20}
+									height={20}
+									alt='Rating icon'
+								/>
+							</span>
 						</div>
 					</div>
 				</a>
